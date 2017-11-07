1084db6a09485341a3f255944cb0a4b272ff1343
Stephen Fairbanks <stephen@ghosthorses.co.uk>
2017-11-07 19:34:55 +0000

    Feat: initial commit, basic setup

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Set vars
+const stage = document.getElementsByClassName('stage')[0];
+const header = document.getElementsByClassName('header')[0];
+
+window.onload = function () {
+  stage.classList.add('ready');
+}
